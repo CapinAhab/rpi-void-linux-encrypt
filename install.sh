400eb91b47e6fa53fe4f94118f8723f3a7fda938
CapinAhab <156573833+CapinAhab@users.noreply.github.com>
2025-05-14 09:25:30 +0000

    Dracut keeps generating for rpi kernel no matter what
    Going to try Uboot with base aarch64 rootfs

--- install.sh
+++ install.sh
@@ -42,7 +42,7 @@ echo $(lsblk -n -o UUID /dev/rpiroot/swap)	/              	ext4      	rw        
 echo $(lsblk -n -o UUID /dev/rpiroot/root)	none           	swap      	defaults  	0 0 >> /mnt/etc/fstab
 
 #Setup boot options
-echo "initramfs initrd.img followkernel" >> /mnt/boot/config.txt
+echo "initramfs initramfs.img followkernel" >> /mnt/boot/config.txt
 #Setup crypttab
 echo rpiroot /dev/mmcblk0p2 /boot/volume.key luks >> /mnt/etc/crypttab
 
@@ -50,12 +50,14 @@ echo rpiroot /dev/mmcblk0p2 /boot/volume.key luks >> /mnt/etc/crypttab
 truncate -s 0 /mnt/boot/cmdline.txt
 echo rd.lvm.vg=rpiroot rd.luks.uuid=$(lsblk -n -o UUID /dev/mmcblk0p2) root=/dev/rpiroot/root rw console=ttyAMA0,115200 kgdboc=ttyAMA0,115200 console=tty1 smsc95xx.turbo_mode=N dwc_otg.lpm_enable=0 loglevel=4 elevator=noop >> /mnt/boot/cmdline.txt
 
+
 #Install requirements on the encrypted system
 xchroot /mnt xbps-install -Suvy cryptsetup lvm2 linux-base linux-headers dracut
 
 #Add dropbear ssh setttings to dracut
 cp configs/10-crypt.conf /mnt/etc/dracut.conf.d/
 
+#Generate crypto key
 dd bs=1 count=64 if=/dev/urandom of=/mnt/boot/volume.key
 
 chmod 000 /mnt/boot/volume.key 
@@ -64,13 +66,24 @@ cryptsetup luksAddKey /dev/mmcblk0p2 /mnt/boot/volume.key
 
 #Make sure package is up to date and remove RPI optimisations
 xchroot /mnt xbps-install -Suvy
-xchroot /mnt xbps-remove rpi-base
-xchroot /mnt xbps-remove rpi-kernel
+
+#Backup boot parttion so removing kernel dosn't wipe all driver blobs
+mkdir backup 
+cp -r /mnt/boot/* backup/
+
+#Forcefull remove the OpTiMiZeD kernel
+xchroot /mnt xbps-remove -F rpi-kernel
+
+#Reinstall backup
+rm -rf /mnt/boot/*
+mv backup/* /mnt/boot/
+rm -rf backup
 
 #Generate initramfs
-xchroot depmod $(ls -S /usr/lib/modules/ | tail -1)
-xchroot /mnt dracut -f /boot/initrd.img $(ls -S /usr/lib/modules/ | tail -1)
+xchroot /mnt depmod $(ls -S /lib/modules/ | tail -1)
+xchroot /mnt dracut -f /boot/initramfs.img --kver $(ls -S /lib/modules/ | tail -1)
 
+#xchroot /mnt xbps-install -y rpi-base
 
 #Unmount partitions
 umount /mnt/boot

70fa5858d940190e1442749de0008b7161edfb57
CapinAhab <156573833+CapinAhab@users.noreply.github.com>
2025-05-16 07:35:27 +0000

    Mcuh better when using basic rootfs rather and rpi specific.
    Kernel image is needed but everything works accept for that
    
    Uboot might fix issues booting from a custom kernel image

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ xbps-install parted wget cryptsetup xtools binfmt-support
 ln -s /etc/sv/binfmt-support /var/service/
 
 wget https://repo-default.voidlinux.org/live/current/void-rpi-aarch64-PLATFORMFS-20250202.tar.xz
+wget https://repo-default.voidlinux.org/live/current/void-aarch64-ROOTFS-20250202.tar.xz
 
 #Need total wipe
 wipefs --all /dev/mmcblk0
@@ -34,15 +35,17 @@ mount /dev/rpiroot/root /mnt
 mkdir /mnt/boot
 mount /dev/mmcblk0p1 /mnt/boot
 
-tar xvfp void-rpi-aarch64-PLATFORMFS-20250202.tar.xz -C /mnt #install system
+tar xvfp void-aarch64-ROOTFS-20250202.tar.xz -C /mnt #install system
+tar -xf void-rpi-aarch64-PLATFORMFS-20250202.tar.xz --strip-components=1 -C /mnt ./boot #Install boot firmware
 rm void-rpi-aarch64-PLATFORMFS-20250202.tar.xz
+rm void-aarch64-ROOTFS-20250202.tar.xz
 
 #Create fstab
 echo $(lsblk -n -o UUID /dev/rpiroot/swap)	/              	ext4      	rw        	0 1 >> /mnt/etc/fstab
 echo $(lsblk -n -o UUID /dev/rpiroot/root)	none           	swap      	defaults  	0 0 >> /mnt/etc/fstab
 
 #Setup boot options
-echo "initramfs initramfs.img followkernel" >> /mnt/boot/config.txt
+echo "initramfs initrd.img followkernel" >> /mnt/boot/config.txt
 #Setup crypttab
 echo rpiroot /dev/mmcblk0p2 /boot/volume.key luks >> /mnt/etc/crypttab
 
@@ -50,14 +53,12 @@ echo rpiroot /dev/mmcblk0p2 /boot/volume.key luks >> /mnt/etc/crypttab
 truncate -s 0 /mnt/boot/cmdline.txt
 echo rd.lvm.vg=rpiroot rd.luks.uuid=$(lsblk -n -o UUID /dev/mmcblk0p2) root=/dev/rpiroot/root rw console=ttyAMA0,115200 kgdboc=ttyAMA0,115200 console=tty1 smsc95xx.turbo_mode=N dwc_otg.lpm_enable=0 loglevel=4 elevator=noop >> /mnt/boot/cmdline.txt
 
-
 #Install requirements on the encrypted system
-xchroot /mnt xbps-install -Suvy cryptsetup lvm2 linux-base linux-headers dracut
+xchroot /mnt xbps-install -Suvy cryptsetup lvm2 linux linux-headers dracut
 
 #Add dropbear ssh setttings to dracut
 cp configs/10-crypt.conf /mnt/etc/dracut.conf.d/
 
-#Generate crypto key
 dd bs=1 count=64 if=/dev/urandom of=/mnt/boot/volume.key
 
 chmod 000 /mnt/boot/volume.key 
@@ -66,24 +67,19 @@ cryptsetup luksAddKey /dev/mmcblk0p2 /mnt/boot/volume.key
 
 #Make sure package is up to date and remove RPI optimisations
 xchroot /mnt xbps-install -Suvy
-
-#Backup boot parttion so removing kernel dosn't wipe all driver blobs
-mkdir backup 
-cp -r /mnt/boot/* backup/
-
-#Forcefull remove the OpTiMiZeD kernel
-xchroot /mnt xbps-remove -F rpi-kernel
-
-#Reinstall backup
-rm -rf /mnt/boot/*
-mv backup/* /mnt/boot/
-rm -rf backup
+<<<<<<< HEAD
 
 #Generate initramfs
-xchroot /mnt depmod $(ls -S /lib/modules/ | tail -1)
-xchroot /mnt dracut -f /boot/initramfs.img --kver $(ls -S /lib/modules/ | tail -1)
+xchroot /mnt dracut -f /boot/initramfs.img --kver $(uname -r)
+=======
+xchroot /mnt xbps-remove rpi-base
+xchroot /mnt xbps-remove rpi-kernel
 
-#xchroot /mnt xbps-install -y rpi-base
+#Generate initramfs
+xchroot depmod $(ls -S /usr/lib/modules/ | tail -1)
+xchroot /mnt dracut -f /boot/initrd.img $(ls -S /usr/lib/modules/ | tail -1)
+
+>>>>>>> parent of 400eb91 (Dracut keeps generating for rpi kernel no matter what)
 
 #Unmount partitions
 umount /mnt/boot
